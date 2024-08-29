0fd1fe65e06f340f921ad5f9ab7aab36244b7ab2
jayrajgb <jayrajgborate11@gmail.com>
2024-08-29 21:49:46 +0530

    deployable v1

--- NewsNow/src/data/fetchNews.js
+++ NewsNow/src/data/fetchNews.js
@@ -1,11 +1,11 @@
 const fetchNews = async (url) => {
     const response = await fetch(`${url}`,{method: "POST"})
     const data = await response.json();
-    // console.log("Data: ",data);
+    console.log("Data: ",data);
     // Filter the data
-    const filterData = await data.articles.filter((item)=> item.title !== "[Removed]" || item.description !== "[Removed]");
+    // const filterData = await data.articles.filter((item)=> item.title !== "[Removed]" || item.description !== "[Removed]");
     // console.log("Filtered: ",filterData);
-    return filterData;
+    return data;
 }
 
 export default fetchNews;
