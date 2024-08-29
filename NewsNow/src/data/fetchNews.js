3d6ae66ef202ff1c3c041018b5d4766b374057b7
jayrajgb <jayrajgborate11@gmail.com>
2024-08-29 21:46:31 +0530

    fixed v2

--- NewsNow/src/data/fetchNews.js
+++ NewsNow/src/data/fetchNews.js
@@ -1,5 +1,5 @@
 const fetchNews = async (url) => {
-    const response = await fetch(`${url}`,{method: "GET"})
+    const response = await fetch(`${url}`,{method: "POST"})
     const data = await response.json();
     // console.log("Data: ",data);
     // Filter the data
